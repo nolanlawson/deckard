121a5c2b14da1ab914d28663c698ab71d0a083e0
Nolan Lawson <nolan.lawson@gmail.com>
2015-10-23 15:14:09 -0400

    repro ResourceNotFound

--- src/main/java/com/example/activity/DeckardActivity.java
+++ src/main/java/com/example/activity/DeckardActivity.java
@@ -2,6 +2,8 @@ package com.example.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import com.example.R;
 
 public class DeckardActivity extends Activity {
@@ -10,5 +12,8 @@ public class DeckardActivity extends Activity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.deckard);
+
+    Toast.makeText(this, R.string.helloworld, Toast.LENGTH_LONG).show();
+
   }
 }
